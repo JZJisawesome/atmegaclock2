af1cc318787f097e8a16cff419f5beb370bb4195
JZJisawesome <JZJisawesome123@hotmail.com>
2021-06-20 21:28:23 -0400

    More setup/testing stuffs

--- src/main.c
+++ src/main.c
@@ -5,9 +5,21 @@
  *
 */
 
-//TODO
+#include <stdbool.h>
+#include <avr/io.h>
+#include <util/delay.h>
 
 int main()
 {
+    DDRB |= 1 << 5;
+    
+    while (true)
+    {
+        PORTB |= 1 << 5;
+        _delay_ms(1000);
+        PORTB &= ~(1 << 5);
+        _delay_ms(1000);
+    }
+    
     return 0;//TESTING
 }
